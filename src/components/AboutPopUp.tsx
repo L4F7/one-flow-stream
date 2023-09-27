fde629a2575b75b199de06de848af12b13e892c6
LuisRestrepoV <luardo182@gmail.com>
2023-09-27 15:24:51 -0600

    About
    About

--- src/components/AboutPopUp.tsx
+++ src/components/AboutPopUp.tsx
@@ -1,11 +1,23 @@
-import { bgColor } from '@/app/shared';
-import { Content } from 'next/font/google';
-import React, { useState, useEffect } from 'react';
 import PopUp from 'react-modal';
 
 PopUp.setAppElement('#main');
 
-const popUpStyle: PopUp.Styles = {
+interface Proyecto {
+  Nombre: string;
+  Integrantes: { Nombre: string; Identificacion: string }[];
+}
+
+interface Curso {
+  Nombre: string;
+  Codigo: string;
+  Horario: string;
+  Semestre: string;
+  Año: string;
+  Escuela: string;
+  Universidad: string;
+}
+
+const aboutDialogStyle: PopUp.Styles = {
   overlay: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
@@ -20,20 +32,38 @@ const popUpStyle: PopUp.Styles = {
   },
 };
 
-interface AboutPopUpProps {
-  isOpen: boolean;
-  onClose: () => void
-  data: any;
+interface aboutInfo {
+  Proyecto?: Proyecto;
+  Curso?: Curso;
 }
 
-const AboutPopUp : React.FC<AboutPopUpProps> = ({ isOpen, onClose, data}) => {
-  
-console.log(JSON.stringify(data))
+const readableJSON = (data: aboutInfo): string => {
+  let result = '';
+  if (data.Proyecto) {
+    const { Nombre, Integrantes } = data.Proyecto;
+    result += `\n\nProyecto:\n\n${Nombre}\nIntegrantes:\n\n`;
+    Integrantes.forEach((integrante) => {
+      result += `*  ${integrante.Nombre}\nIdentificación: ${integrante.Identificacion}\n\n`;
+    });
+  }
+  if (data.Curso) {
+    const { Nombre, Codigo, Horario, Semestre, Año, Escuela, Universidad} = data.Curso;
+    result += `Curso: ${Nombre}\n  Código: ${Codigo}\n  Horario: ${Horario}\n  Semestre: ${Semestre}\n  Año: ${Año}\n  Escuela: ${Escuela}\n  Universidad: ${Universidad}\n`;
+  }
+  return result;
+};
 
+const AboutPopUp = ({ isOpen, onClose, data}:{isOpen: boolean; onClose: () => void; data: aboutInfo[];}) => {
+  
   return (
-    <PopUp isOpen={isOpen} onRequestClose={onClose} contentLabel="Popup Modal" style={popUpStyle}>
-      <h2>{data.Nombre}</h2>
-      <h2>{data.Identificacion}</h2>
+    <PopUp isOpen={isOpen} onRequestClose={onClose} contentLabel="Popup Modal" style={aboutDialogStyle}>
+      <div>
+      {data.map((item, index) => (
+        <div key={index}>
+          <pre>{readableJSON(item)}</pre>
+        </div>
+      ))}
+    </div>
       <br></br>
       <button onClick={onClose} className="h-1/10 bg-sky-700 hover:bg-sky-800 text-white font-bold py-2 px-4 rounded">Cerrar</button>
     </PopUp>
