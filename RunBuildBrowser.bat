505f7ed40116f5225a0a935860dfa9a344db6dc5
Luis Fuentes Fuentes <80366974+L4F7@users.noreply.github.com>
2023-11-02 02:52:20 -0600

    .bat that runs buld, start and then opens a browser in localhost:3000

---
+++ RunBuildBrowser.bat
@@ -0,0 +1,9 @@
+@echo off
+start cmd /k "swipl simple_service_server.pl"
+start cmd /k "npm run build && npm start"
+
+:: Add a delay to allow time for the server to start
+ping 127.0.0.1 -n 10 > nul
+
+:: Open the browser to localhost:3000
+start "" "http://localhost:3000"
